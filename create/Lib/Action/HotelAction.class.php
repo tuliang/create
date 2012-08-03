2cf376df953b040f5695e034c35e09d7a6413cfb
tuliang <info@tuliang.org>
2012-08-03 18:29:19 +0800

    完成FaceBook酒店代码生成器

---
+++ create/Lib/Action/HotelAction.class.php
@@ -0,0 +1,86 @@
+<?php
+class HotelAction extends Action {
+	
+	//初始化
+	protected function _initialize() {
+		header("Content-Type:text/html; charset=utf-8");
+		header('P3P: CP=CAO PSA OUR');
+	}
+	
+    public function index(){
+    	
+    	$this->display();
+    }
+    
+	public function createCode(){
+		
+		if (!empty($_POST)) {
+			//拿到POST值 
+			$post = $this->cleanPostValue();
+			//将名称转为小写并且将单词用下划线连接
+			$post['hotel_name'] = str_replace(' ', '_', strtolower($post['hotel_name']));
+			//定义模板地址
+			define('TPL_PATH', APP_PATH.'templates/hotel/');
+			//读取模板
+			$text = file_get_contents(TPL_PATH.'index.html');
+		
+			//替换文本
+			foreach ($post as $key=>$item){
+				$text = str_replace('{'.$key.'}', $item, $text);
+			}
+		
+			//定义模板生成地址
+			define('CREATE_PATH', APP_PATH.'result/HotelCode/'.$post['hotel_name'].'/');
+		
+			//创建目录
+			mkdir(CREATE_PATH, 0777, true);
+			//创建文件
+			file_put_contents(CREATE_PATH.'index.html', $text);
+			file_put_contents(CREATE_PATH.$post['hotel_name'].'txt', $text);
+			 
+			//创建其他文件
+			mkdir(CREATE_PATH.'css/', 0777, true);
+			file_put_contents(CREATE_PATH.'css/style.css', file_get_contents(TPL_PATH.'css/style.css'));
+		
+			mkdir(CREATE_PATH.'images/', 0777, true);
+			file_put_contents(CREATE_PATH.'images/left_button.png', file_get_contents(TPL_PATH.'images/left_button.png'));
+			file_put_contents(CREATE_PATH.'images/right_button.png', file_get_contents(TPL_PATH.'images/right_button.png'));
+			$this->upload();//上传图片
+			 
+			$this->success('创建成功', '__URL__/index');
+		}else{
+			$this->error('创建失败', '__URL__/index');
+		}
+	}
+	
+	//清理POST中的无关值
+	private function cleanPostValue(){
+	
+		unset($_POST['__hash__']);
+	
+		return $_POST;
+	}
+	
+	//上传图片
+	private function upload() {
+	
+		if (!empty($_POST)) {
+	
+			import("@.ORG.UploadFile");
+			$upload = new UploadFile('', 'jpg,gif,png,jpeg', '', CREATE_PATH.'images/', 'picture');
+			//开启缩略图功能
+			$upload->thumb = true;
+			//缩略图前缀
+			$upload->thumbPrefix = '';
+			//缩略图大小
+			$upload->thumbMaxWidth = '185';
+			$upload->thumbMaxHeight = '220';
+	
+			if (!$upload->upload()) {
+				$this->error($upload->getErrorMsg());
+			}
+		}else{
+			$this->error('创建失败', '__URL__/index');
+		}
+	} 
+}
