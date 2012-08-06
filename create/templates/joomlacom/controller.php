530390f974e924376e631c95c93c7a8cb4dc16a6
tuliang <info@tuliang.org>
2012-08-06 15:37:35 +0800

    优化Joomla组件代码生成器

--- create/templates/joomlacom/controller.php
+++ create/templates/joomlacom/controller.php
@@ -4,11 +4,6 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 jimport('joomla.application.component.controller');
 
 class {com_name}Controller extends JController{
-	
-	function __construct($config = array()){
-		
-		parent::__construct($config);
-	}
 
     function display($tpl = null){
 		
